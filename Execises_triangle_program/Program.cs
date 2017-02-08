f09764b95ca38e547600564eed327c87bdd284a6
l <l@DESKTOP-ASASGJL>
2017-02-08 08:43:30 +0100

    Add project files.

---
+++ Execises_triangle_program/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Execises_triangle_program
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.Title = "Triangle Identifier";
+            Triangle_checker triangle_checker = new Triangle_checker();
+            int a;
+            int b;
+            int c;
+            string side1;
+            string side2;
+            string side3;
+
+            while (true)
+            {
+                Console.WriteLine("side 1: ");
+                side1 = Console.ReadLine();
+                if (!int.TryParse(side1, out a))
+                {
+                    Console.WriteLine("invalid input");
+                    continue;
+                }
+
+                Console.WriteLine("side 2: ");
+                side2 = Console.ReadLine();
+                if (!int.TryParse(side2, out b))
+                {
+                    Console.WriteLine("invalid input");
+                    continue;
+                }
+
+                Console.WriteLine("side 3: ");
+                side3 = Console.ReadLine();
+                if (!int.TryParse(side3, out c))
+                {
+                    Console.WriteLine("invalid input");
+                    continue;
+                }
+                Console.WriteLine(triangle_checker.IdentifyTriangle(a, b, c));
+            }
+        }
+    }
+}
---
+++ Execises_triangle_program/Triangle_checker.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Execises_triangle_program
+{
+    public class Triangle_checker
+    {
+        public Triangle_checker()
+        {
+        }
+
+        /// <summary>
+        /// Will identify if the 3 side will give a valid triangle, if valid it will tell what type of triangle it is.
+        /// </summary>
+        /// <param name="side1"></param>
+        /// <param name="side2"></param>
+        /// <param name="side3"></param>
+        /// <returns></returns>
+        public string IdentifyTriangle(int side1, int side2, int side3)
+        {
+            if (IsVaildInput(side1, side2, side3))
+            {
+                // All three sides has equal length
+                if (side1 == side2 && side2 == side3)
+                {
+                    return "This is an Equilateral triangle";
+                }
+                // Two sides has equal length
+                else if (side1 == side2 && side2 != side3 || side2 == side3 && side3 != side1 || side1 == side3 && side3 != side2)
+                {
+                    return "This is an Isosceles triangle";
+                }
+                // All sides has different length
+                else if (side1 != side2 && side2 != side3 && side1 != side3)
+                {
+                    return "This is a Scalene triangle";
+                }
+            }
+
+            return "This is not a valid triangle";
+        }
+
+        /// <summary>
+        /// Check to see if the 3 side can form a triangle
+        /// </summary>
+        /// <returns></returns>
+        private bool IsVaildInput(int a, int b, int c)
+        {
+            //The triangle inequality Theorem stats that if this is true, then the three side from a valid triangle
+            if (a + b > c && a + c > b && b + c > a)
+            {
+                return true;
+            }
+            return false;
+        }
+    }
+}
